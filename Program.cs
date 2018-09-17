d05a71b765633b14c9a7c3c514be9aa87c02cdb6
Anastasija Pischik <nastijpischik@gmail.com>
2018-09-17 22:08:18 +0300

    v1

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace Reflection
+{
+    class Program
+    {
+        [Serializable]
+        class Test
+        {
+            public string Subject { get; set; }
+            public int SMark { get; set; }
+
+            public Test()
+            {
+                SMark = 0;
+            }
+
+            public Test(string s, int n)
+            {
+                Subject = s;
+                SMark = n;
+            }
+            public int TestMethod(int n)
+            {
+                return n + 10;
+            }
+        }
+
+        class Student
+        {
+            public string FullName { set; get; }
+            public int[] Marks { set; get; }
+
+            public Student()
+            {
+                Marks = new int[5];
+            }
+
+            public Student(int n)
+            {
+                Marks = new int[n];
+            }
+
+            public void GetAvgMark(out double avgMark)
+            {
+                avgMark = 0.0;
+                for (int i = 0; i < Marks.Length; i++)
+                    avgMark += Marks[i];
+                avgMark = avgMark / Marks.Length;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TypeInfo info = new TypeInfo(typeof(Test));
+            info.ShowTypeInfo();
+            TypeInfo studInfo = new TypeInfo(typeof(Student));
+            studInfo.ShowTypeInfo();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TypeInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Reflection
+{
+    public class TypeInfo
+    {
+        private Type type;
+        public TypeInfo(Type type)
+        {
+            this.type = type;
+        }
+        
+        public IEnumerable<CustomAttributeData> Attributes => type.CustomAttributes;
+        public IEnumerable<FieldInfo> Fields => type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        public IEnumerable<MethodInfo> Methods => type.GetMethods(BindingFlags.Public | BindingFlags.Instance).Where(mi => !mi.IsSpecialName);
+        public IEnumerable<ConstructorInfo> Constructors => type.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+        public IEnumerable<PropertyInfo> Properties => type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+
+        public void ShowTypeInfo ()
+        {
+            Console.WriteLine("Type info about {0}", this.type.Name);
+            Console.WriteLine("Attributes");
+            foreach (var atr in this.Attributes)
+            {
+                Console.WriteLine("  " + atr.AttributeType);
+            }
+            Console.WriteLine("Fields");
+            foreach (var fieldInfo in this.Fields)
+            {
+                Console.WriteLine("   {0}: {1}", fieldInfo.Name, fieldInfo.FieldType);
+            }
+            Console.WriteLine("Properties");
+            foreach (var prop in this.Properties)
+            {
+                Console.WriteLine("   {0} ({1})", prop.Name, prop.PropertyType.Name);
+            }
+            Console.WriteLine("Constructors");
+            foreach (var constr in this.Constructors)
+            {
+                Console.WriteLine("   Constructors: " + constr.ToString());
+            }
+            Console.WriteLine("Methods");
+            foreach (var meth in this.Methods)
+            {
+                Console.WriteLine("   name - " + meth.Name + "; return value - " + meth.ReturnType);
+            }
+        }
+
+    }
+}
